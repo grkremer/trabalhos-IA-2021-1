19bba788e567dd5eeb9d5c9f615b0d96cbd94c75
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2021-08-26 21:55:25 -0300

    Sucessor
    Terminei o bgl ai e ja era

--- hello.py
+++ hello.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 
 def troca(palavra, a, b):
     palavra = list(palavra)
@@ -8,34 +6,55 @@ def troca(palavra, a, b):
 
 def sucessor(estado):
     posicao = acha_underline(estado)
-    return ( move_esquerda(estado,posicao), move_direita(estado,posicao), move_cima(estado,posicao), move_baixo(estado, posicao) )
+    solucoes = []
+    if(pode_mover_baixo(estado, posicao)):
+        solucoes.append(("abaixo", move_baixo(estado, posicao)))
+    if(pode_mover_cima(estado, posicao)):
+        solucoes.append(("acima", move_cima(estado, posicao)))
+    if(pode_mover_esquerda(estado, posicao)):
+        solucoes.append(("esquerda", move_esquerda(estado, posicao)))
+    if(pode_mover_direita(estado, posicao)):
+        solucoes.append(("direita", move_direita(estado, posicao)))
+    return solucoes
 
 def acha_underline(estado):
     return estado.index('_')
 
+def pode_mover_cima(estado, posicao):
+    return posicao-3 >= 0
+
+def pode_mover_baixo(estado, posicao):
+    return posicao+3 < 9
+
+def pode_mover_esquerda(estado, posicao):
+    return posicao != 0 and posicao != 3 and posicao != 6
+
+def pode_mover_direita(estado, posicao):
+    return posicao != 2 and posicao != 5 and posicao != 8
+
 def move_cima(estado,posicao):
-    if posicao-3 >= 0:
+    if pode_mover_cima(estado, posicao):
         return troca(estado, posicao, posicao-3)
     else:
-        return ''
+        return estado
 
 def move_baixo(estado,posicao):
-    if posicao+3 < 9:
+    if pode_mover_baixo(estado, posicao):
         return troca(estado, posicao, posicao+3)
     else:
-        return ''
+        return estado
 
 def move_direita(estado,posicao):
-    if posicao != 2 and posicao != 5 and posicao != 8:
+    if pode_mover_direita(estado, posicao):
         return troca(estado, posicao, posicao+1)
     else:
-        return ''
+        return estado
 
 def move_esquerda(estado,posicao):
-    if posicao != 0 and posicao != 3 and posicao != 6:
+    if pode_mover_esquerda(estado, posicao):
         return troca(estado, posicao, posicao-1)
     else:
-        return ''
+        return estado
 
 
 def print_estado(estado):
@@ -47,6 +66,7 @@ caso = "12_345678"
 print_estado(caso)
 print("___")
 print(sucessor(caso))
+
 
 
 
--- solucao.py
+++ solucao.py
@@ -13,18 +13,56 @@ class Nodo:
         # substitua a linha abaixo pelo seu codigo
         raise NotImplementedError
 
-
+def troca(palavra, a, b):
+    palavra = list(palavra)
+    palavra[a], palavra[b] = palavra[b], palavra[a]
+    return ''.join(palavra)
 def sucessor(estado):
-    """
-    Recebe um estado (string) e retorna uma lista de tuplas (ação,estado atingido)
-    para cada ação possível no estado recebido.
-    Tanto a ação quanto o estado atingido são strings também.
-    :param estado:
-    :return:
-    """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
-
+    posicao = acha_underline(estado)
+    solucoes = []
+    if(pode_mover_baixo(estado, posicao)):
+        solucoes.append(("abaixo", move_baixo(estado, posicao)))
+    if(pode_mover_cima(estado, posicao)):
+        solucoes.append(("acima", move_cima(estado, posicao)))
+    if(pode_mover_esquerda(estado, posicao)):
+        solucoes.append(("esquerda", move_esquerda(estado, posicao)))
+    if(pode_mover_direita(estado, posicao)):
+        solucoes.append(("direita", move_direita(estado, posicao)))
+    return solucoes
+def acha_underline(estado):
+    return estado.index('_')
+def pode_mover_cima(estado, posicao):
+    return posicao-3 >= 0
+def pode_mover_baixo(estado, posicao):
+    return posicao+3 < 9
+def pode_mover_esquerda(estado, posicao):
+    return posicao != 0 and posicao != 3 and posicao != 6
+def pode_mover_direita(estado, posicao):
+    return posicao != 2 and posicao != 5 and posicao != 8
+def move_cima(estado,posicao):
+    if pode_mover_cima(estado, posicao):
+        return troca(estado, posicao, posicao-3)
+    else:
+        return estado
+def move_baixo(estado,posicao):
+    if pode_mover_baixo(estado, posicao):
+        return troca(estado, posicao, posicao+3)
+    else:
+        return estado
+def move_direita(estado,posicao):
+    if pode_mover_direita(estado, posicao):
+        return troca(estado, posicao, posicao+1)
+    else:
+        return estado
+def move_esquerda(estado,posicao):
+    if pode_mover_esquerda(estado, posicao):
+        return troca(estado, posicao, posicao-1)
+    else:
+        return estado
+def print_estado(estado):
+    print(estado[0:3])
+    print(estado[3:6])
+    print(estado[6:9])
 
 def expande(nodo):
     """
